158801631ebb2f822fc5f1ee71490eebabc9c59f
Swen Kooij <swenkooij@gmail.com>
2013-01-31 14:31:28 +0100

    Added documentation to WhatsConstants.cs

--- WhatsAppApi/Settings/WhatsConstants.cs
+++ WhatsAppApi/Settings/WhatsConstants.cs
@@ -6,24 +6,74 @@ using System.Text;
 
 namespace WhatsAppApi.Settings
 {
+    /// <summary>
+    /// Holds constant information used to connect to whatsapp server
+    /// </summary>
     class WhatsConstants
     {
         #region ServerConstants
+
+        /// <summary>
+        /// The whatsapp digest
+        /// </summary>
         public const string WhatsAppDigest = "xmpp/s.whatsapp.net";
+
+        /// <summary>
+        /// The whatsapp host
+        /// </summary>
         public const string WhatsAppHost = "bin-short.whatsapp.net";
+
+        /// <summary>
+        /// The whatsapp XMPP realm
+        /// </summary>
         public const string WhatsAppRealm = "s.whatsapp.net";
+
+        /// <summary>
+        /// The whatsapp server
+        /// </summary>
         public const string WhatsAppServer = "s.whatsapp.net";
+
+        /// <summary>
+        /// The whatsapp group chat server
+        /// </summary>
         public const string WhatsGroupChat = "g.us";
+
+
+        /// <summary>
+        /// The whatsapp version the client complies to
+        /// </summary>
         public const string WhatsAppVer = "2.8.7";
+
+        /// <summary>
+        /// The port that needs to be connected to
+        /// </summary>
         public const int WhatsPort = 5222;
 
+        /// <summary>
+        /// iPhone device
+        /// </summary>
         public const string IphoneDevice = "iPhone";
+
+        /// <summary>
+        /// The useragent used for http requests
+        /// </summary>
         public const string UserAgend = "WhatsApp/2.8.7 iPhone_OS/6.1.0 Device/iPhone_4S";
+
+        /// <summary>
+        /// The whatsapp build hash
+        /// </summary>
         public const string WhatsBuildHash = "889d4f44e479e6c38b4a834c6d8417815f999abe";
         #endregion
 
         #region ParserConstants
+        /// <summary>
+        /// The number style used
+        /// </summary>
         public static NumberStyles WhatsAppNumberStyle = (NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign);
+
+        /// <summary>
+        /// Unix epoch DateTime 
+        /// </summary>
         public static DateTime UnixEpoch = new DateTime(0x7b2, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         #endregion
     }
