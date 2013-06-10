e3abe97c3ad1e92e681035be902529e25f7bff34
Max Kovaljov <shirioko@gmail.com>
2013-06-10 18:07:02 +0200

    Partially fixed media upload

--- WhatsAppApi/WhatsApp.cs
+++ WhatsAppApi/WhatsApp.cs
@@ -7,6 +7,7 @@ using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
+using System.Web.Script.Serialization;
 using WhatsAppApi.Helper;
 using WhatsAppApi.Parser;
 using WhatsAppApi.Response;
@@ -320,6 +321,7 @@ namespace WhatsAppApi
         /// <param name="icon">Icon</param>
         public void MessageImage(string to, string filepath)
         {
+            to = this.GetJID(to);
             FileInfo finfo = new FileInfo(filepath);
             string type = string.Empty;
             switch (finfo.Extension)
@@ -350,10 +352,18 @@ namespace WhatsAppApi
             }
 
             //request upload
-            string url = this.UploadFile(filehash, "image", finfo.Length, filepath, to, type);
+            UploadResponse response = this.UploadFile(filehash, "image", finfo.Length, filepath, to, type);
+
+            if (response != null && !String.IsNullOrEmpty(response.url))
+            {
+                //send message
+                FMessage msg = new FMessage(to, true) { key = { id = TicketManager.GenerateId() }, media_wa_type = FMessage.Type.Image, media_mime_type = response.mimetype, media_name = response.url.Split('/').Last(), media_size = response.size, media_url = response.url };
+                this.WhatsSendHandler.SendMessage(msg);
+            }
+
         }
 
-        private string UploadFile(string b64hash, string type, long size, string path, string to, string contenttype)
+        private UploadResponse UploadFile(string b64hash, string type, long size, string path, string to, string contenttype)
         {
             ProtocolTreeNode media = new ProtocolTreeNode("media", new KeyValue[] {
                 new KeyValue("xmlns", "w:m"),
@@ -375,78 +385,138 @@ namespace WhatsAppApi
                 i++;
                 this.PollMessages();
             }
-            try
+            if (this.uploadResponse != null && this.uploadResponse.GetChild("duplicate") != null)
             {
-                string uploadUrl = this.uploadResponse.GetChild("media").GetAttribute("url");
+                UploadResponse res = new UploadResponse(this.uploadResponse);
                 this.uploadResponse = null;
-
-                Uri uri = new Uri(uploadUrl);
-
-                string hashname = string.Empty;
-                byte[] buff = MD5.Create().ComputeHash(System.Text.Encoding.Default.GetBytes(path));
-                StringBuilder sb = new StringBuilder();
-                foreach (byte b in buff)
-                {
-                    sb.Append(b.ToString("X2"));
-                }
-                hashname = String.Format("{0}.{1}", sb.ToString(), path.Split('.').Last());
-
-                string boundary = "zzXXzzYYzzXXzzQQ";
-
-                sb = new StringBuilder();
-
-                sb.AppendFormat("--{0}\r\n", boundary);
-                sb.Append("Content-Disposition: form-data; name=\"to\"\r\n\r\n");
-                sb.AppendFormat("{0}\r\n", to);
-                sb.AppendFormat("--{0}\r\n", boundary);
-                sb.Append("Content-Disposition: form-data; name=\"from\"\r\n\r\n");
-                sb.AppendFormat("{0}\r\n", this.phoneNumber);
-                sb.AppendFormat("{0}\r\n", boundary);
-                sb.AppendFormat("Content-Disposition: form-data; name=\"file\"; filename=\"{0}\"\r\n", hashname);
-                sb.AppendFormat("Content-Type: {0}\r\n\r\n", contenttype);
-                string header = sb.ToString();
-
-                sb = new StringBuilder();
-                sb.AppendFormat("\r\n--{0}--\r\n", boundary);
-                string footer = sb.ToString();
-
-                long clength = size + header.Length + footer.Length;
-
-                sb = new StringBuilder();
-                sb.AppendFormat("POST {0}\r\n", uploadUrl);
-                sb.AppendFormat("Content-Type: multipart/form-data; boundary={0}\r\n", boundary);
-                sb.AppendFormat("Host: {0}\r\n", uri.Host);
-                sb.AppendFormat("User-Agent: {0}\r\n", WhatsConstants.UserAgent);
-                sb.AppendFormat("Content-Length: {0}\r\n\r\n", clength);
-                string post = sb.ToString();
-
-                TcpClient tc = new TcpClient(uri.Host, 443);
-                SslStream ssl = new SslStream(tc.GetStream());
+                return res;
+            }
+            else
+            {
                 try
                 {
-                    ssl.AuthenticateAsClient(uri.Host);
+                    string uploadUrl = this.uploadResponse.GetChild("media").GetAttribute("url");
+                    this.uploadResponse = null;
+
+                    Uri uri = new Uri(uploadUrl);
+
+                    string hashname = string.Empty;
+                    byte[] buff = MD5.Create().ComputeHash(System.Text.Encoding.Default.GetBytes(path));
+                    StringBuilder sb = new StringBuilder();
+                    foreach (byte b in buff)
+                    {
+                        sb.Append(b.ToString("X2"));
+                    }
+                    hashname = String.Format("{0}.{1}", sb.ToString(), path.Split('.').Last());
+
+                    string boundary = "zzXXzzYYzzXXzzQQ";
+
+                    sb = new StringBuilder();
+
+                    sb.AppendFormat("--{0}\r\n", boundary);
+                    sb.Append("Content-Disposition: form-data; name=\"to\"\r\n\r\n");
+                    sb.AppendFormat("{0}\r\n", to);
+                    sb.AppendFormat("--{0}\r\n", boundary);
+                    sb.Append("Content-Disposition: form-data; name=\"from\"\r\n\r\n");
+                    sb.AppendFormat("{0}\r\n", this.phoneNumber);
+                    sb.AppendFormat("--{0}\r\n", boundary);
+                    sb.AppendFormat("Content-Disposition: form-data; name=\"file\"; filename=\"{0}\"\r\n", hashname);
+                    sb.AppendFormat("Content-Type: {0}\r\n\r\n", contenttype);
+                    string header = sb.ToString();
+
+                    sb = new StringBuilder();
+                    sb.AppendFormat("\r\n--{0}--\r\n", boundary);
+                    string footer = sb.ToString();
+
+                    long clength = size + header.Length + footer.Length;
+
+                    sb = new StringBuilder();
+                    sb.AppendFormat("POST {0}\r\n", uploadUrl);
+                    sb.AppendFormat("Content-Type: multipart/form-data; boundary={0}\r\n", boundary);
+                    sb.AppendFormat("Host: {0}\r\n", uri.Host);
+                    sb.AppendFormat("User-Agent: {0}\r\n", WhatsConstants.UserAgent);
+                    sb.AppendFormat("Content-Length: {0}\r\n\r\n", clength);
+                    string post = sb.ToString();
+
+                    TcpClient tc = new TcpClient(uri.Host, 443);
+                    SslStream ssl = new SslStream(tc.GetStream());
+                    try
+                    {
+                        ssl.AuthenticateAsClient(uri.Host);
+                    }
+                    catch (Exception e)
+                    {
+                        throw e;
+                    }
+
+                    //HttpWebRequest req = WebRequest.Create(uploadUrl) as HttpWebRequest;
+                    //set post headers
+                    //req.ContentType = String.Format("multipart/form-data; boundary={0}\r\n", boundary);
+                    //req.UserAgent = WhatsConstants.UserAgent;
+                    //Stream reqStream = req.GetRequestStream();
+
+                    List<byte> buf = new List<byte>();
+                    buf.AddRange(Encoding.UTF8.GetBytes(post));
+                    buf.AddRange(Encoding.UTF8.GetBytes(header));
+                    buf.AddRange(File.ReadAllBytes(path));
+                    buf.AddRange(Encoding.UTF8.GetBytes(footer));
+
+                    ssl.Write(buf.ToArray(), 0, buf.ToArray().Length);
+
+                    //moment of truth...
+                    buff = new byte[1024];
+                    ssl.Read(buff, 0, 1024);
+
+                    string result = Encoding.UTF8.GetString(buff);
+                    foreach (string line in result.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        if (line.StartsWith("{"))
+                        {
+                            string fooo = line.TrimEnd(new char[] { (char)0 });
+                            JavaScriptSerializer jss = new JavaScriptSerializer();
+                            UploadResponse resp = jss.Deserialize<UploadResponse>(fooo);
+                            if (!String.IsNullOrEmpty(resp.url))
+                            {
+                                return resp;
+                            }
+                        }
+                    }
                 }
                 catch (Exception e)
                 {
-                    throw e;
+                    //Console.WriteLine(e.Message);
                 }
-                byte[] msgdata = Encoding.ASCII.GetBytes(post);
-                ssl.Write(msgdata);
-                msgdata = Encoding.ASCII.GetBytes(header);
-                ssl.Write(msgdata);
-                buff = File.ReadAllBytes(path);
-                ssl.Write(buff);
-                msgdata = Encoding.ASCII.GetBytes(footer);
-                ssl.Write(msgdata);
-
-                //moment of truth...
-                ssl.Read(buff, 0, buff.Length);
-                string json = System.Text.Encoding.ASCII.GetString(buff);
-                return json;
             }
-            catch (Exception e)
-            { }
             return null;
+        }
+
+        public class UploadResponse
+        {
+            public string url { get; set; }
+            public string mimetype { get; set; }
+            public int size { get; set; }
+            public string filehash { get; set; }
+            public string type { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+
+            public UploadResponse()
+            { }
+
+            public UploadResponse(ProtocolTreeNode node)
+            {
+                node = node.GetChild("duplicate");
+                if (node != null)
+                {
+                    this.url = node.GetAttribute("url");
+                    this.mimetype = node.GetAttribute("mimetype");
+                    this.size = Int32.Parse(node.GetAttribute("size"));
+                    this.filehash = node.GetAttribute("filehash");
+                    this.type = node.GetAttribute("type");
+                    this.width = Int32.Parse(node.GetAttribute("width"));
+                    this.height = Int32.Parse(node.GetAttribute("height"));
+                }
+            }
         }
 
         /// <summary>
@@ -625,7 +695,7 @@ namespace WhatsAppApi
                     }
                     if (ProtocolTreeNode.TagEquals(node, "iq")
                         && node.GetAttribute("type").Equals("result", StringComparison.OrdinalIgnoreCase)
-                        && ProtocolTreeNode.TagEquals(node.children.First(), "media")
+                        && (ProtocolTreeNode.TagEquals(node.children.First(), "media") || ProtocolTreeNode.TagEquals(node.children.First(), "duplicate"))
                         )
                     {
                         //media upload
