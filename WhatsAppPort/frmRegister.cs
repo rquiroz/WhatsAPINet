97a04a3dae3306da7a9d365d0e01973ba4126371
Max Kovaljov <shirioko@gmail.com>
2013-08-20 13:08:56 +0200

    Added code register functionality
    and made cc and phone into properties to remove duplicate code

--- WhatsAppPort/frmRegister.cs
+++ WhatsAppPort/frmRegister.cs
@@ -12,6 +12,8 @@ namespace WhatsAppPort
     public partial class frmRegister : Form
     {
         public string number;
+        protected string cc;
+        protected string phone;
         public string password;
 
         public frmRegister(string number)
@@ -20,6 +22,8 @@ namespace WhatsAppPort
             if (!string.IsNullOrEmpty(number))
             {
                 this.number = number;
+                this.cc = this.number.Substring(0, 2);
+                this.phone = this.number.Substring(2);
                 this.txtPhoneNumber.Text = number;
             }
         }
@@ -34,9 +38,7 @@ namespace WhatsAppPort
                     method = "voice";
                 }
                 this.number = this.txtPhoneNumber.Text;
-                string cc = this.number.Substring(0, 2);
-                string phone = this.number.Substring(2);
-                if (WhatsAppApi.Register.WhatsRegisterV2.RequestCode(cc, phone, method))
+                if (WhatsAppApi.Register.WhatsRegisterV2.RequestCode(this.cc, this.phone, method))
                 {
                     this.grpStep1.Enabled = false;
                     this.grpStep2.Enabled = true;
@@ -46,13 +48,21 @@ namespace WhatsAppPort
 
         private void btnRegisterCode_Click(object sender, EventArgs e)
         {
-            this.grpStep2.Enabled = false;
-            this.grpResult.Enabled = true;
+            if (!String.IsNullOrEmpty(this.txtCode.Text) && this.txtCode.Text.Length == 6)
+            {
+                string code = this.txtCode.Text;
+                this.password = WhatsAppApi.Register.WhatsRegisterV2.RegisterCode(this.cc, this.phone, code);
+                if (!String.IsNullOrEmpty(this.password))
+                {
+                    this.txtOutput.Text = String.Format("Found password:\r\n{0}\r\n\r\nWrite it down and click Done to proceed", this.password);
+                    this.grpStep2.Enabled = false;
+                    this.grpResult.Enabled = true;
+                }
+            }
         }
 
         private void btnDone_Click(object sender, EventArgs e)
         {
-            this.password = "Derp";
             this.DialogResult = System.Windows.Forms.DialogResult.OK;
         }
     }
