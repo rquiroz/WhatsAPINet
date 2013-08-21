7eb1c4694a1283fbd6312e5b438ebc4f08cfa944
Max Kovaljov <shirioko@gmail.com>
2013-08-21 14:27:34 +0200

    Updated WhatsAppReg to work with updated code

--- WhatsAppReg/frmRegister.cs
+++ WhatsAppReg/frmRegister.cs
@@ -33,7 +33,7 @@ namespace WhatsAppReg
                 this.number = this.txtPhoneNumber.Text;
                 this.cc = this.number.Substring(0, 2);
                 this.phone = this.number.Substring(2);
-                if (WhatsAppApi.Register.WhatsRegisterV2.RequestCode(this.cc, this.phone, method))
+                if (WhatsAppApi.Register.WhatsRegisterV2.RequestCode(this.cc, this.phone, out this.password, method))
                 {
                     this.grpStep1.Enabled = false;
                     this.grpStep2.Enabled = true;
