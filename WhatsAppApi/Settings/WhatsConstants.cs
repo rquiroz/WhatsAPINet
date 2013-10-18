e69d36810ace9e7c0f3bd45f09f0bbe059390de0
Max Kovaljov <shirioko@gmail.com>
2013-10-18 13:00:07 +0200

    Updated client details to 2.11.102

--- WhatsAppApi/Settings/WhatsConstants.cs
+++ WhatsAppApi/Settings/WhatsConstants.cs
@@ -42,7 +42,7 @@ namespace WhatsAppApi.Settings
         /// <summary>
         /// The whatsapp version the client complies to
         /// </summary>
-        public const string WhatsAppVer = "2.11.69";
+        public const string WhatsAppVer = "2.11.102";
 
         /// <summary>
         /// The port that needs to be connected to
@@ -57,7 +57,7 @@ namespace WhatsAppApi.Settings
         /// <summary>
         /// The useragent used for http requests
         /// </summary>
-        public const string UserAgent = "WhatsApp/2.11.69 Android/4.2.1 Device/GalaxyS3";
+        public const string UserAgent = "WhatsApp/2.11.102 Android/4.2.1 Device/GalaxyS3";
 
         #endregion
 
