5320da2cfa260815c9d49a38d0c8b6316696a3a5
shirioko <maxkovaljov@outlook.com>
2014-03-22 21:09:34 +0100

    Res bump to 2.11.200

--- WhatsAppApi/Settings/WhatsConstants.cs
+++ WhatsAppApi/Settings/WhatsConstants.cs
@@ -42,7 +42,7 @@ namespace WhatsAppApi.Settings
         /// <summary>
         /// The whatsapp version the client complies to
         /// </summary>
-        public const string WhatsAppVer = "2.11.151";
+        public const string WhatsAppVer = "2.11.200";
 
         /// <summary>
         /// The port that needs to be connected to
@@ -57,7 +57,7 @@ namespace WhatsAppApi.Settings
         /// <summary>
         /// The useragent used for http requests
         /// </summary>
-        public const string UserAgent = "WhatsApp/2.11.151 Android/4.2.1 Device/GalaxyS3";
+        public const string UserAgent = "WhatsApp/2.11.200 Android/4.4.2 Device/GalaxyS4";
 
         #endregion
 
